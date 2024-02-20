ac2b1b7532bed7dd4d4b3ed4d9c0c76218336249
Ivan Yurygin <sivan2140@gmail.com>
2024-02-20 19:30:26 +0300

    convertToHtmlAdded

--- src/components/Note/AllNotes.tsx
+++ src/components/Note/AllNotes.tsx
@@ -5,16 +5,22 @@ import { useGetAllNotesQuery } from '../../services/notesApi'
 import { useContext, useState } from 'react'
 import { CustomContext } from '../../context/UserContext'
 import SidebarNote from './SidebarNote'
+import { CiCirclePlus } from "react-icons/ci"
+import { useNavigate } from 'react-router-dom'
 
 const AllNotes = () => {
 
 	const {user} = useContext(CustomContext)
 	const { data: notes } = useGetAllNotesQuery(String(user.user.id))
+	const navigate = useNavigate()
 	const [isOpen, setIsOpen] = useState<boolean>(true)
 
 	return (
 		<div className="allNotes">
-			<h3 onClick={() => setIsOpen(!isOpen)} className='allNotes__title'><span><CiCircleList /></span>All notes</h3>
+			<h3 onClick={() => setIsOpen(!isOpen)} className='allNotes__title'>
+				<span><CiCircleList /></span>All notes
+				<span onClick={() => navigate('/notes/createNote')} id='plus'><CiCirclePlus /></span>
+			</h3>
 			{isOpen && (<ul className='allNotes__body'>
 				{notes && notes.map((note: NoteT) => (
 					<SidebarNote note={note} key={note.id} />
--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,29 +1,39 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { NoteT } from "../../types/types"
 import { useGetNoteQuery } from "../../services/notesApi"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { CustomContext } from "../../context/UserContext"
+import '../../styles/notePage.scss'
+import { ContentState, convertFromRaw } from "draft-js"
+import { stateToHTML } from "draft-js-export-html"
 
 
 const Note = () => {
 
 	const { noteId } = useParams()
 	const { user } = useContext(CustomContext)
-	const {data: note} = useGetNoteQuery({userId: String(user.user.id), noteId: String(noteId)})
+	const { data: note } = useGetNoteQuery({ userId: String(user.user.id), noteId: String(noteId) })
 	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (note) {
+			const savedContent = note.content
+			const contentState: ContentState = convertFromRaw(JSON.parse(savedContent as string))
+			const html = stateToHTML(contentState)
+			const noteContent = document.getElementById('note-content')
+			if (noteContent) {
+				noteContent.innerHTML = html
+			}
+		}
+	}, [note])
 
 
 	return (
-		<div>
-			<div>
-				<button onClick={() => navigate('/notes/createNote')}>Создать</button>
+		<div className="note">
+			<h2 className="note__title">{note?.title}</h2>
+			<div className="note__content" id="note-content">
+
 			</div>
-			{note && 
-				<>
-					<h1>{note.title}</h1>
-					<p>{note.content}</p>
-				</>
-			}
 		</div>
 	)
 }
